d3c78bb8f38e84e489707036e4aff575d3fab01d
finnete kawira <finnetekawira@gmail.com>
2021-01-31 21:36:27 +0300

    commit this

---
+++ ajax.js
@@ -0,0 +1,15 @@
+
+
+
+    function votersAge(adult){
+        if (adult){
+            var name ="finnete"
+            var age = "27"
+
+            console.log(`The voter ${name} is of age ${age} and is eligible`)
+        }
+    }
+    votersAge(`king`)
+
+   
+    
